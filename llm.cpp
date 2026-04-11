3ca9ea14cf1de14fdc8798e692dbbbff621701b3
Your Name <you@example.com>
2026-04-12 01:31:40 +0200

    httpbin

--- llm.cpp
+++ llm.cpp
@@ -1,22 +1,19 @@
-#include <iostream>
+#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
+#include <print>
 #include <replxx.hxx>
 #include <string>
+
+using json = nlohmann::json;
 
 int main() {
   replxx::Replxx rx;
   rx.install_window_change_handler(); // Automatically handles terminal resizing
   rx.set_max_history_size(100);
 
-  std::string prompt = "λ ";
+  const std::string prompt = "λ ";
 
-  while (true) {
-    const char *c_input = rx.input(prompt);
-
-    if (c_input == nullptr) {
-      // User pressed Ctrl+D (EOF) or Ctrl+C
-      break;
-    }
-
+  while (const char *c_input = rx.input(prompt)) {
     std::string input{c_input};
 
     if (input.empty())
@@ -26,8 +23,22 @@ int main() {
 
     rx.history_add(input);
 
-    // Process the user's input
-    std::cout << "You sent: \n" << input << "\n\n";
+    json req_body = {{"message", std::move(input)}};
+    auto res = cpr::Post(
+        cpr::Url{"https://httpbin.org/post"}, cpr::Body{req_body.dump()},
+        cpr::Header{{"Content-Type", "application/json"}});
+
+    if (res.status_code == 200) {
+      try {
+        auto res_json = json::parse(res.text);
+        std::print("API Resp:\n {}\n\n", res_json["json"].dump(2));
+      } catch (const json::parse_error &err) {
+        std::print("Failed to parse API Resp: {}\n\n", err.what());
+      }
+    } else {
+      std::print("API Req Failed!\nStatus code: {}\nError: {}\n\n",
+                 res.status_code, res.error.message);
+    }
   }
 
   return 0;
